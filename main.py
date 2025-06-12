5b39dc7a753bc49def8a634ffdad7f2208550af9
wonseok289 <ws030902@gmail.com>
2025-06-12 14:34:18 +0900

    menu

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="<Enter your API key here>",
+  api_key="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",
 )
 
 messages = [{
     'role': 'system',
-    'content': 'You are a helpful interview assistant. If you are given type of interview(ex: soccer club, AI graduate school), \
+    'content': 'You are a helpful interview assistant. \
+                If you are given type of interview(ex: soccer club, AI graduate school), \
                 you say "OK, I\'m ready." and you are ready to start the interview about given type.(Don\'t reply except "OK, I\'m ready.") \
                 And then whenever I ask you a question, you will ask the one interview question. \
-                If I reply to your question, you will evaluate my answer(rating: 1점 ~ 5점) and give me the feedback. \
+                If I reply to your question, you will evaluate my answer(점수: 1점 ~ 5점) and give me the feedback. \
+                If I say "I want to quit", you will say "Thank you for your time. Goodbye." and end the interview. \
                 And you must reply in Korean. \
                 '
 }]
@@ -35,22 +37,16 @@ else:
     print(completion.choices[0].message.content)
 
 
-    # while True:
-    #     msg = input(' ')
+    while True:
+        ask = input('Want question? ')
 
-    #     if msg.strip().lower() == 'q':
-    #         break
+        if ask.strip().lower() == 'y':
+            pass
+        elif ask.strip().lower() == 'q':
+            break
+        else:
+            continue
 
-    #     messages.append({
-    #         'role': 'user',
-    #         'content': msg
-    #     })
+        
 
-    #     completion = client.chat.completions.create(
-    #         model="qwen/qwen3-14b:free",
-    #         messages=messages
-    #     )
-    #     messages.append(completion.choices[0].message)
-    #     print(completion.choices[0].message.content)
 
-    # print()
